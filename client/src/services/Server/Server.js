7a367a2a4cf8f20407fa98b9af19b11f4cbbd6c8
ArtemOL <artemolin19@gmail.com>
2026-09-20 19:10:19 +0400

    Сделал

---
+++ client/src/services/Server/Server.js
@@ -0,0 +1,44 @@
+import Store from "../Store";
+
+export default class Server {   
+
+    constructor(store){
+        this.store = store;
+    }
+
+    async request(method, params) {
+        try {
+            params.method = method;
+
+            const url = `${this.HOST}/?${Object.keys(params).map(key => `${key}=${params[key]}`).join('&')}`;
+
+            const response = await fetch(url);
+            const answer = await response.json();
+
+
+            if (answer.result === 'ok' && answer.data) {
+                return answer.data;
+            }
+
+            return null;
+        } catch (e) {
+            console.log('Request exception:', e);
+            return null;
+        }
+    }
+
+    async getCitiesList(){
+        const response = await this.request('getCitiesList');
+        if (!response) {
+            return null;
+        }
+
+        console.log('Cities from server:', response);
+        return response;
+    }
+
+    async sendCity(city) {
+        this.request('sendCity', {city});
+    }
+
+}
