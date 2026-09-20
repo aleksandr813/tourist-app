f6d41bc927ef6c6ab827c6f76d8e99bfe2ab6344
Alexey <alexiyshatkov2@gmail.com>
2026-09-20 14:17:35 +0400

    modified:   server/application/router/handlers/useChooseCityHandler.js

--- server/application/router/handlers/useChooseCityHandler.js
+++ server/application/router/handlers/useChooseCityHandler.js
@@ -2,8 +2,11 @@ module.exports = (mediator, answer) => {
     const { SECLECT_CITY } = mediator.getEventTypes();
     return (req, res) => {
         const city = req.params.city;
+
         if (!city){
-            res.send(answer.bad(67));
+
+            return res.send(answer.bad(67));
         }
+        return res.send(answer.good(true));
     };
 };
