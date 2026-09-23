6c0287030a6e1ffb92f5978daac0a71506d5ec38
aleksandr813 <aleksandrtrushnikov18@rambler.ru>
2026-09-23 23:02:44 +0400

    modified:   server/application/router/handlers/index.js

--- server/application/router/handlers/index.js
+++ server/application/router/handlers/index.js
@@ -1,8 +1,11 @@
 const useChooseCityHandler = require("./useChooseCityHandler");
 const notFoundHandler = require("./notFoundHandler");
+const useGetCitiesHandler = require("./useGetCitiesHandler");
+const useGetRoutesHandler = require("./useGetRoutesHandler");
 
 module.exports = {
     notFoundHandler,
     useChooseCityHandler,
-
+    useGetCitiesHandler,
+    useGetRoutesHandler,
 };
