4bf9c2a8c802e42011ba13555ba82f93f63dc5e3
aleksandr813 <aleksandrtrushnikov18@rambler.ru>
2026-09-23 23:00:28 +0400

    modified:   server/app.js 	modified:   server/application/modules/BaseManager.js

--- server/application/modules/BaseManager.js
+++ server/application/modules/BaseManager.js
@@ -1,9 +1,10 @@
 class BaseManager {
     constructor (params) {
-        const { mediator, db } = params;
+        const { mediator, db, common } = params;
 
         this.mediator = mediator;
         this.db = db;
+        this.common = common;
 
         this.EVENTS = mediator.getEventTypes();
         this.TRIGGERS = mediator.getTriggerTypes();
